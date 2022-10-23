a01590196c5a9398ab63c3dca87936718781501f
Vivek <vivekraj78965.vr@gmail.com>
2022-10-23 18:49:08 +0530

    general class in js

--- first example/app.js
+++ first example/app.js
@@ -1,13 +1,13 @@
 "use strict";
 exports.__esModule = true;
-// function call(a):number{
-//     return a;
-// }
-// console.log(call("vivek"));
-//as the type of function is number and we are passing string to it 
-//Although this will not generate any error because the number is type of function not for the argument
-//if we define type as number for the argument , then the above code will produce an error
-function call(a) {
-    return a;
-}
-console.log(call("vivek"));
+var App = /** @class */ (function () {
+    function App() {
+        this.name = "anil";
+    }
+    App.prototype.getName = function () {
+        console.log(this.name);
+    };
+    return App;
+}());
+var a1 = new App;
+a1.getName();
--- first example/app.ts
+++ first example/app.ts
@@ -1,19 +1,9 @@
 export{}; // export is used to solve duplicate identifier issue
-// function call(a):number{
-//     return a;
-// }
-
-// console.log(call("vivek"));
-
-//as the type of function is number and we are passing string to it 
-//Although this will not generate any error because the number is type of function not for the argument
-
-
-
-
-//if we define type as number for the argument , then the above code will produce an error
-function call(a:number):number{
-    return a;
+class App{
+    name="anil";
+    getName(){
+        console.log(this.name);
+    }
 }
-
-console.log(call("vivek"));
+ let a1 = new App;
+ a1.getName();
